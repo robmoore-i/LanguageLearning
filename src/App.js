a6bef350a40ce9dd69d24dcef0b9528f941e72ee
Rob <robmoore121@gmail.com>
2018-09-29 09:57:13 +0100

    rename project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import georgianFlag from './georgianFlag.svg'
+import globe from './globe.svg'
 import './App.css'
 
 class App extends Component {
@@ -7,9 +7,9 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={georgianFlag} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Kartvelang</h1>
-                    <h2 className="App-description">Step-by-step Georgian lessons in the browser</h2>
+                    <img src={globe} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Melange</h1>
+                    <h2 className="App-description">Step-by-step language lessons in the browser</h2>
                     <a className="Call-to-action-btn">Get Started</a>
                 </header>
             </div>
