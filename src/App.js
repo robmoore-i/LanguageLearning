1d8965804d75f1c12a303a9357f30ca08c925707
Rob <robmoore121@gmail.com>
2018-09-29 11:55:21 +0100

    Simplified entry point

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+// React
 import React, {Component} from 'react'
-import { Link, Route } from 'react-router-dom';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
+// Service worker
+import registerServiceWorker from "./registerServiceWorker";
+
+// Resources
 import globe from './globe.svg'
 import './App.css'
+
+export function startApp() {
+    ReactDOM.render(<App/>, document.getElementById('root'));
+    registerServiceWorker();
+}
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Route exact path="/" component={Home}/>
-                <Route path="/lesson-map" component={LessonMap}/>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/lesson-map" component={LessonMap}/>
+                    </Switch>
+                </BrowserRouter>
             </div>
         )
     }
@@ -22,9 +37,7 @@ class Home extends Component {
                 <img src={globe} className="App-logo" alt="logo"/>
                 <h1 className="App-title">Melange</h1>
                 <h2 className="App-description">Step-by-step language lessons in the browser</h2>
-                <Link to="lesson-map" className="Call-to-action-btn">
-                    Get Started
-                </Link>
+                <Link to="lesson-map" className="Call-to-action-btn">Get Started</Link>
             </header>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {startApp} from './App';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
-    document.getElementById('root'));
-registerServiceWorker();
+startApp()
