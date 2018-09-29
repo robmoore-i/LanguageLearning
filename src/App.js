0fb44a6d975effa37213e536fb0755782ccc42c3
Rob <robmoore121@gmail.com>
2018-09-29 10:34:45 +0100

    Add routing from home to lesson-map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import { Link, Route } from 'react-router-dom';
+
 import globe from './globe.svg'
 import './App.css'
 
@@ -6,12 +8,35 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={globe} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Melange</h1>
-                    <h2 className="App-description">Step-by-step language lessons in the browser</h2>
-                    <a className="Call-to-action-btn">Get Started</a>
-                </header>
+                <Route exact path="/" component={Home}/>
+                <Route path="/lesson-map" component={LessonMap}/>
+            </div>
+        )
+    }
+}
+
+class Home extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src={globe} className="App-logo" alt="logo"/>
+                <h1 className="App-title">Melange</h1>
+                <h2 className="App-description">Step-by-step language lessons in the browser</h2>
+                <Link to="lesson-map" className="Call-to-action-btn">
+                    Get Started
+                </Link>
+            </header>
+        )
+    }
+}
+
+class LessonMap extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    This is the lesson map
+                </p>
             </div>
         )
     }
