1241725ad05bf983f12cfb0ec1ae1159761d6ca6
Rob <robmoore121@gmail.com>
2018-09-30 11:40:15 +0100

    Show courses on the lesson map

--- src/main/LessonMap.js
+++ src/main/LessonMap.js
@@ -3,15 +3,39 @@ import React, {Component} from "react"
 
 // Resources
 import '../styles/LessonMap.css'
+import georgia from '../images/flagGeorgia.svg'
+import germany from '../images/flagGermany.svg'
 
 export default class LessonMap extends Component {
     render() {
-        return (
-            <div className="Lesson-map">
-                <p>
-                    This is the lesson map
-                </p>
-            </div>
-        )
+        return [
+            <header className="Lesson-map-header">
+                <h1 className="Lesson-map-title">Choose a course</h1>
+            </header>,
+
+            <body className="Lesson-map-body">
+                <div className="Lesson-map">
+                    <CourseButton name="Georgian" src={georgia}/>
+                    <CourseButton name="German" src={germany}/>
+                </div>
+            </body>
+        ]
+    }
+}
+
+class CourseButton extends Component {
+    render() {
+        return [
+            <div>
+                <a>
+                    <h3 className="Course-title">{this.props.name}</h3>
+                    <img className="Course-icon"
+                         alt={this.props.name}
+                         src={this.props.src}/>
+                </a>
+            </div>,
+            <br />,
+            <br />
+        ]
     }
 }
