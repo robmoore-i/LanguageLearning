b43474fcbdc4749d1604d8d3e99c4dfe1c2b2a0f
Rob <robmoore121@gmail.com>
2018-09-30 17:13:36 +0100

    Delete unused mount-style test utils

--- src/test/utils.js
+++
@@ -1,9 +0,0 @@
-function followLink(link) {
-    link.simulate('click', { button: 0 })
-}
-
-function assertHasChild(component, selector) {
-    expect(component.find(selector)).toHaveLength(1)
-}
-
-module.exports = { followLink, assertHasChild }
