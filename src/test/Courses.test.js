d789f514382bcdf01703bf66e50a3fa66fc8ba55
Rob <robmoore121@gmail.com>
2018-09-30 17:08:29 +0100

    test for german course button

--- src/test/Courses.test.js
+++ src/test/Courses.test.js
@@ -18,3 +18,10 @@ it('directs the user to courses/georgian upon pressing the [georgian flag]', () 
     let georgianCourseLink = georgianCourseButton.find('#course-link-georgian').first()
     expect(georgianCourseLink.is('[to="courses/georgian"]')).toBe(true)
 })
+
+it('directs the user to courses/german upon pressing the [german flag]', () => {
+    let testCoursesPage = shallow(<Courses />)
+    let germanCourseButton = testCoursesPage.find('#course-button-german').dive()
+    let germanCourseLink = germanCourseButton.find('#course-link-german').first()
+    expect(germanCourseLink.is('[to="courses/german"]')).toBe(true)
+})
