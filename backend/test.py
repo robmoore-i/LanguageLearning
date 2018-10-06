affb880b3494498b56258541d078092fd3994d22
Rob <robmoore121@gmail.com>
2018-10-06 13:23:24 +0100

    Can nadle options http reqs for cors

--- backend/test.py
+++ backend/test.py
@@ -1,34 +1,48 @@
 import os
 import time
+
 import requests
 from assertpy import assert_that
 
 QHOME = os.environ["QHOME"]
 
+
 def start_backend_q():
-  os.system(QHOME + "/l32/q backend.q &")
+    os.system(QHOME + "/l32/q backend.q &")
 
 
 def kill_backend_q():
-  os.system("pkill -f \"" + QHOME + "/l32/q backend.q\"")
+    os.system("pkill -f \"" + QHOME + "/l32/q backend.q\"")
 
 
 def lesson_names():
-  res = requests.get("http://localhost:8000/lessonnames")
-  assert_that(res.status_code).is_equal_to(200)
-  assert_that(res.json()).is_equal_to(["hello", "what are you called?"])
+    res = requests.get("http://localhost:8000/lessonnames")
+    assert_that(res.status_code).is_equal_to(200)
+    assert_that(res.json()).is_equal_to(["hello", "what are you called?"])
+
+
+def lesson_names_options():
+    options = requests.options("http://localhost:8000/lessonnames")
+    assert_that(options.headers["Access-Control-Allow-Origin"]).is_equal_to("http://localhost:3000")
+    assert_that(options.headers["Access-Control-Allow-Methods"]).is_equal_to("GET, POST")
+    assert_that(options.headers["Access-Control-Allow-Headers"]).is_equal_to("Content-Type")
+    assert_that(options.status_code).is_equal_to(200)
 
 
 def run_test(test_name, test):
-  try:
-    print("- " + test_name)
-    test()
-  except AssertionError as e:
-    print("fail\n\t" + str(e))
+    try:
+        print("- " + test_name)
+        test()
+    except AssertionError as e:
+        print("fail\n\t" + str(e))
+    except Exception as e:
+        print("error\n\t" + str(e))
+
 
 
 def tests():
-  run_test("lesson_names", lesson_names)
+    run_test("lesson_names", lesson_names)
+    run_test("lesson_names_options", lesson_names_options)
 
 
 def main():
