cc943edab9b39856fa40bd64a479f8614b82e18c
Rob <robmoore121@gmail.com>
2018-10-06 14:42:16 +0100

    Get npm to shut up on start

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 # Backend
 echo "Starting backend"
 cd backend/
-$QHOME/l32/q backend.q &
+$QHOME/l32/q backend.q > ../backend.log &
 
 # Frontend
 echo "Starting frontend"
 cd ../frontend
-npm start &
+npm start > ../frontend.log &
 cd ..
 
 echo "Done"
