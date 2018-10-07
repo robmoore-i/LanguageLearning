42f1f71ebbfad57ad390d12a8322eea8b39a488a
Rob <robmoore121@gmail.com>
2018-10-07 13:03:45 +0100

    Rm cypress unfortunately

--- test/cypress/integration/fetch_lesson_names_spec.js
+++
@@ -1,6 +0,0 @@
-it("/courses/georgian -- contains the available Georgian lessons", () => {
-    cy.visit("http://localhost:3000/courses/georgian")
-
-    let lessonButtons = cy.get(".Lesson-list").children()
-    lessonButtons.should('have.length', 2)
-})
