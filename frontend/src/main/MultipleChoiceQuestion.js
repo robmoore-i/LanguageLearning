35006b9aa8ab2e6bc20b19922a7cf930542c1d73
Rob <robmoore121@gmail.com>
2018-10-17 11:43:56 +0100

    Style MCQs

--- frontend/src/main/MultipleChoiceQuestion.js
+++ frontend/src/main/MultipleChoiceQuestion.js
@@ -16,16 +16,18 @@ export default class MultipleChoiceQuestion extends Component {
     }
 
     MultipleChoiceCheckBox(choice) {
-        return [
-            <input id={"choicebox-" + choice}
-                   type="radio"
-                   key="checkbox"
-                   checked={this.state.activeChoice === choice}
-                   onChange={() => {
-                       this.setState({activeChoice: choice})
-                   }}/>,
-            <span id={"choiceValue-" + choice} key="choiceValue">{this.props.q[choice]}</span>
-        ]
+        return (
+            <div className="choice">
+                <input id={"choicebox-" + choice}
+                       type="radio"
+                       key="checkbox"
+                       checked={this.state.activeChoice === choice}
+                       onChange={() => {
+                           this.setState({activeChoice: choice})
+                       }}/>
+                <span id={"choiceValue-" + choice} key="choiceValue">{this.props.q[choice]}</span>
+            </div>
+        )
     }
 
     mark(choice) {
@@ -40,17 +42,23 @@ export default class MultipleChoiceQuestion extends Component {
 
     render() {
         return [
-            <div key="question-title">
+            <br key="header--break--question-title"/>,
+            <div id="question-title" key="question-title">
                 <span id="question">{"Which of these " + this.props.q.question + "?"}</span>
                 <span id={this.state.markResult.id}>
                     <img src={this.state.markResult.img} className="question-result" alt="mark-result-status" />
                 </span>
             </div>,
+            <br key="question-title--break--choices" />,
             <div key="choices">
                 {this.MultipleChoiceCheckBox(Choices.A)}
                 {this.MultipleChoiceCheckBox(Choices.B)}
                 {this.MultipleChoiceCheckBox(Choices.C)}
                 {this.MultipleChoiceCheckBox(Choices.D)}
+            </div>,
+            <div key="choices--break--submit-button">
+                <br />
+                <br />
             </div>,
             <a id="submit-for-marking-button" key="submit-for-marking-button"
                onClick={() => {
