9d8da523a82a5e191ec0e140a41de62cabbf9d4a
Rob <robmoore121@gmail.com>
2018-10-18 22:04:41 +0100

    Goes to next question using completionListener on MCQ

--- frontend/src/main/Lesson.js
+++ frontend/src/main/Lesson.js
@@ -1,9 +1,10 @@
 // React
-import React, {Component} from "react";
+import React, {Component} from "react"
 // Resources
 import '../styles/Lesson.css'
 // Main
-import MultipleChoiceQuestion from "./MultipleChoiceQuestion";
+import MultipleChoiceQuestion from "./MultipleChoiceQuestion"
+import TranslationQuestion from "./TranslationQuestion"
 
 export default class Lesson extends Component {
     constructor(props) {
@@ -12,9 +13,18 @@ export default class Lesson extends Component {
         this.server = this.props.server
         this.courseName = this.props.courseName
 
+        let tq = {type: 0, given: "hello", answer: "გამარჯობა"}
+        let TQ = <TranslationQuestion q={tq} key="question"/>
+
+        let mcq = {type: 1, question: "sounds like \"i\" in English", a: "ა", b: "ო", c: "უ", d: "ი", answer: "d"}
+        let MCQ = <MultipleChoiceQuestion q={mcq} key="question" completionListener={this.questionCompleted()} />
+
+        this.questions = [MCQ, TQ]
+
         this.state = {
             lessonName: this.props.lessonNameInUrl, // Placeholder until server response.
-            loaded: false
+            loaded: false,
+            currentQuestionIndex: 0
         }
     }
 
@@ -28,6 +38,19 @@ export default class Lesson extends Component {
         })
     }
 
+    questionCompleted() {
+        const setState = this.setState.bind(this) // Bind 'this' reference for use within callback
+        return () => {
+            setState((state) => {
+                return {currentQuestionIndex: state.currentQuestionIndex + 1}
+            })
+        }
+    }
+
+    currentQuestion() {
+        return this.questions[this.state.currentQuestionIndex]
+    }
+
     renderLoading() {
         return (
             <h1>Loading {this.courseName}: {this.state.lessonName}</h1>
@@ -37,12 +60,11 @@ export default class Lesson extends Component {
     renderLoaded() {
         let capitalise = (s => s[0].toUpperCase() + s.substring(1))
         let capitalisedCourseName = capitalise(this.courseName)
-        let q = {type: 1, question: "sounds like \"i\" in English", a: "ა", b: "ო", c: "უ", d: "ი", answer: "d"}
         return [
             <header className="Lesson-header" key="header">
                 <h1 className="Lesson-title">{capitalisedCourseName}: {this.state.lessonName}</h1>,
             </header>,
-            <MultipleChoiceQuestion q={q} key="question" completionListener={() => {}} />
+            this.currentQuestion()
         ]
     }
 
