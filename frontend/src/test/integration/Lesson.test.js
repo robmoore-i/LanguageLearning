8bd37702195fb3416040b9cb6aea97cd8acbe188
Rob <robmoore121@gmail.com>
2018-10-21 15:20:27 +0100

    Added integration test for repeating an MCQ

--- frontend/src/test/integration/Lesson.test.js
+++ frontend/src/test/integration/Lesson.test.js
@@ -20,8 +20,8 @@ let mockServer = lesson => {
 }
 
 it('can advance through an MCQ and TQ', async () => {
-    let tq = {type: 0, given: "hello", answer: "გამარჯობა"}
     let mcq = {type: 1, question: "sounds like \"i\" in English", a: "ა", b: "ო", c: "უ", d: "ი", answer: "d"}
+    let tq = {type: 0, given: "hello", answer: "გამარჯობა"}
     let testServer = mockServer({name: "Hello!", questions: [mcq, tq]})
     let testLesson = mount(<Lesson courseName="georgian" lessonNameInUrl="hello" server={testServer} />)
     await sleep(mockServerLoadTimeMs)
@@ -33,6 +33,33 @@ it('can advance through an MCQ and TQ', async () => {
     testLesson.find("#continue-button").simulate("click")
 
     // TQ
+    testLesson.find("#answer-input-textbox").simulate("change", textBoxInputEvent("გამარჯობა"))
+    testLesson.find("#submit-for-marking-button").simulate("click")
+    testLesson.find("#continue-button").simulate("click")
+
+    expect(testLesson.find("div").text()).toEqual("Ya Dun m8")
+})
+
+it('can repeats a TQ if answered incorrectly', async () => {
+    let tq = {type: 0, given: "hello", answer: "გამარჯობა"}
+    let mcq = {type: 1, question: "sounds like \"i\" in English", a: "ა", b: "ო", c: "უ", d: "ი", answer: "d"}
+    let testServer = mockServer({name: "Hello!", questions: [tq, mcq]})
+    let testLesson = mount(<Lesson courseName="georgian" lessonNameInUrl="hello" server={testServer} />)
+    await sleep(mockServerLoadTimeMs)
+    testLesson.update()
+
+    // TQ Incorrect
+    testLesson.find("#answer-input-textbox").simulate("change", textBoxInputEvent("wrong answer"))
+    testLesson.find("#submit-for-marking-button").simulate("click")
+    testLesson.find("#answer-input-textbox").simulate("change", textBoxInputEvent("გამარჯობა"))
+    testLesson.find("#continue-button").simulate("click")
+
+    // MCQ
+    testLesson.find("#choice-d").simulate("click")
+    testLesson.find("#submit-for-marking-button").simulate("click")
+    testLesson.find("#continue-button").simulate("click")
+
+    // TQ Repeated
     testLesson.find("#answer-input-textbox").simulate("change", textBoxInputEvent("გამარჯობა"))
     testLesson.find("#submit-for-marking-button").simulate("click")
     testLesson.find("#continue-button").simulate("click")
