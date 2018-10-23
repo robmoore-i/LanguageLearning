d0e71154e2635a8a75715a6b50c275926d1c0345
Rob <robmoore121@gmail.com>
2018-10-23 23:14:38 +0100

    Parallel impl: Server is now a tested object and provides an interface just of questions, regardless of whether they were fetched directly or created from fetched productions

--- frontend/src/main/server.js
+++ frontend/src/main/server.js
@@ -15,3 +15,51 @@ const server = {
 }
 
 export default server
+
+const DefaultFetcher = {
+    fetchJSON: (url) => {
+        return fetch(url).then(response => response.json())
+    }
+}
+
+export function Server(fetcher) {
+    return {
+        fetchLessonNames: () => {
+            return fetcher.fetchJSON(config.backendOrigin + "/lessonnames")
+        },
+
+        fetchLesson: (lessonNameInUrl) => {
+            return fetcher.fetchJSON(config.backendOrigin + "/lesson/" + lessonNameInUrl).then(lesson => {
+                if ("productionInputsPairs" in lesson) {
+                    let productions = lesson.productionInputsPairs.productions
+                    let inputs = lesson.productionInputsPairs.inputs
+
+                    if (productions.length !== inputs.length) {
+                        throw new Error("Lesson.componentDidMount: Productions and Inputs from server have unequal lengths - cannot make questions")
+                    }
+
+                    let questions = []
+
+                    for (let i = 0; i < inputs.length; i++) {
+                        const input = inputs[i];
+                        const production = productions[i]
+                        for (let j = 0; j < input.length; j++) {
+                            questions.push(production.using(input[j]))
+                        }
+                    }
+                    return {
+                        name: lesson.name,
+                        questions: questions
+                    }
+                } else {
+                    return {
+                        name: lesson.name,
+                        questions: lesson.questions
+                    }
+                }
+            })
+        }
+    }
+}
+
+export const DefaultServer = Server(DefaultFetcher)
---
+++ frontend/src/test/unit/server.test.js
@@ -0,0 +1,62 @@
+import {TranslationProduction} from "../../main/TranslationProduction";
+import {ObjectBuilder} from "../../main/ObjectBuilder";
+import ProductionVariable from "../../main/ProductionVariable";
+import {Server} from "../../main/server";
+
+it('Creates questions itself when sent productions from the server', () => {
+    let tp = TranslationProduction((name) => "Hello " + name, (name) => "გამარჯობა " + name, [ProductionVariable.NAME])
+    let input = ObjectBuilder().put(ProductionVariable.NAME, "Rob").build()
+    let mockFetcher = {
+        fetchJSON: (url) => {
+            return new Promise(resolve => resolve(
+                {
+                    name: "Hello!",
+                    productionInputsPairs: {
+                        productions: [tp],
+                        inputs: [[input]]
+                    }
+                }
+            ))
+        }
+    }
+    let testServer = Server(mockFetcher)
+
+    testServer.fetchLesson("some_lesson").then(lesson => {
+        expect(lesson.name).toEqual("Hello!")
+        expect(lesson.questions).toEqual([tp.using(input)])
+    })
+})
+
+it('Returns the sent questions when sent questions explicitly', () => {
+    let mcq = {type: 1, question: "sounds like \"i\" in English", a: "ა", b: "ო", c: "უ", d: "ი", answer: "d"}
+    let tq = {type: 0, given: "hello", answer: "გამარჯობა"}
+    let mockFetcher = {
+        fetchJSON: (url) => {
+            return new Promise(resolve => resolve(
+                {
+                    name: "Server Stuff",
+                    questions: [mcq, tq]
+                }
+            ))
+        }
+    }
+    let testServer = Server(mockFetcher)
+
+    testServer.fetchLesson("ayylmao").then(lesson => {
+        expect(lesson.name).toEqual("Server Stuff")
+        expect(lesson.questions).toEqual([mcq, tq])
+    })
+})
+
+it('Returns lesson names as given by server', () => {
+    let mockFetcher = {
+        fetchJSON: (url) => {
+            return new Promise(resolve => resolve(["A", "B", "C"]))
+        }
+    }
+    let testServer = Server(mockFetcher)
+
+    testServer.fetchLessonNames().then(lessonNames => {
+        expect(lessonNames).toEqual(["A", "B", "C"])
+    })
+})
