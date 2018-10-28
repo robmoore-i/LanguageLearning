25df4f9dbfd54b5c43d02a95a9d8a03cf0459681
Rob <robmoore121@gmail.com>
2018-10-28 21:15:24 +0000

    Move handlers in order in which they are usually called

--- backend/src/handlers.go
+++ backend/src/handlers.go
@@ -6,6 +6,19 @@ import (
 	"log"
 )
 
+func GetCourses(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	courses := QueryCourses()
+
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(courses); err != nil {
+		panic(err)
+	}
+}
+
 func GetLessonNames(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
@@ -40,19 +53,6 @@ func GetLesson(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(lesson); err != nil {
-		panic(err)
-	}
-}
-
-func GetCourses(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-
-	courses := QueryCourses()
-
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(courses); err != nil {
 		panic(err)
 	}
 }
