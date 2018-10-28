48b31535f8c165bf05d92ca6c30eb77bd07628e4
Rob <robmoore121@gmail.com>
2018-10-28 12:49:37 +0000

    ReadingQuestion marks correct answers as correct

--- frontend/src/main/ReadingQuestion.js
+++ frontend/src/main/ReadingQuestion.js
@@ -2,16 +2,19 @@
 import React, {Component} from "react"
 // Resources
 import '../styles/Question.css'
+import Mark from "./Mark";
+import {submitForMarkingButton} from "./Question";
 
 export default class ReadingQuestion extends Component {
-    questions() {
-        return this.props.q.questions.map((q, i) => {
-            return <div id={"question-" + String(i+1) + "-given"} key={"question-" + String(i+1) + "-given"}>
-                {q.given}
-            </div>
-        })
-    }
+    constructor(props) {
+        super(props)
 
+        this.state = {
+            currentAnswers: (new Array(this.props.q.questions.length)).fill(""),
+            marked: false,
+            marks: (new Array(this.props.q.questions.length)).fill(Mark.UNMARKED)
+        }
+    }
     render() {
         return [
             <div id="question-source" key="question-source">
@@ -19,7 +22,65 @@ export default class ReadingQuestion extends Component {
             </div>,
             <div key="questions">
                 {this.questions()}
-            </div>
+            </div>,
+            this.button()
         ]
+    }
+
+    questions() {
+        return this.props.q.questions.map((q, i) => {
+            return this.PosedQuestion(i, q)
+        })
+    }
+
+    // The (i)th posed question, (q), about the extract
+    PosedQuestion(i, q) {
+        return [
+            <div id={"question-given-" + String(i)} key={"question-given-" + String(i)}>
+                {q.given}
+            </div>,
+            <span id={this.state.marks[i].id + "-" + String(i)} key={this.state.marks[i].id + "-" + String(i)}>
+                    <img src={this.state.marks[i].img} className="question-result" alt="mark-result-status" />
+            </span>,
+            this.answerInputTextBox(i)
+        ]
+    }
+
+    answerInputTextBox(n) {
+        let onChange = (event) => {
+            this.setState((state) => {
+                let newAnswers = state.currentAnswers
+                newAnswers[n] = event.target.value
+                return {currentAnswers: newAnswers}
+            })
+        }
+
+        return <textarea id={"answer-input-textbox-" + String(n)} key={"answer-input-textbox-" + String(n)}
+                                      rows="5" cols="50"
+                                      onChange={onChange}/>;
+    }
+
+    button() {
+        const setState = this.setState.bind(this) // Bind 'this' reference for use within callback.
+        return submitForMarkingButton(() => {
+            setState({
+                marked: true,
+                marks: this.markAnswers()})
+        })
+    }
+
+    // Matches currentAnswers against this.props.q.questions
+    markAnswers() {
+        let marks = (new Array(this.props.q.questions.length)).fill(Mark.UNMARKED)
+        for (let i = 0; i < this.state.currentAnswers.length; i++) {
+            let actual = this.state.currentAnswers[i]
+            let expected = this.props.q.questions[i].answer
+            if (actual === expected) {
+                marks[i] = Mark.CORRECT
+            } else {
+                marks[i] = Mark.INCORRECT
+            }
+        }
+        return marks
     }
 }
--- frontend/src/test/unit/ReadingQuestion.test.js
+++ frontend/src/test/unit/ReadingQuestion.test.js
@@ -5,7 +5,7 @@ import {mount, shallow} from 'enzyme'
 // Main
 import ReadingQuestion from '../../main/ReadingQuestion'
 // Enzyme react-adapter configuration & others
-import {configureAdapter} from "../utils"
+import {configureAdapter, textBoxInputEvent} from "../utils"
 
 configureAdapter()
 
@@ -34,6 +34,25 @@ it('Shows all the questions', () => {
     }
     let testRQ = shallow(<ReadingQuestion q={q} />)
 
-    expect(testRQ.find("#question-1-given").text()).toBe("Where did Vlad go?")
-    expect(testRQ.find("#question-2-given").text()).toBe("What did he get there?")
+    expect(testRQ.find("#question-given-0").text()).toBe("Where did Vlad go?")
+    expect(testRQ.find("#question-given-1").text()).toBe("What did he get there?")
+})
+
+it('Marks correct answers as correct', () => {
+    let q = {
+        type: 2,
+        source: "Vlad went to the kitchen and got some cake",
+        questions: [
+            {given: "Where did Vlad go?", answer: "Kitchen"},
+            {given: "What did he get there?", answer: "Cake"}
+        ]
+    }
+    let testRQ = mount(<ReadingQuestion q={q} />)
+
+    testRQ.find("#answer-input-textbox-0").simulate("change", textBoxInputEvent("Kitchen"))
+    testRQ.find("#answer-input-textbox-1").simulate("change", textBoxInputEvent("Cake"))
+    testRQ.find("#submit-for-marking-button").simulate("click")
+
+    expect(testRQ.find("#question-result-correct-0").exists()).toBe(true)
+    expect(testRQ.find("#question-result-correct-1").exists()).toBe(true)
 })
