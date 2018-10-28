16289cbc4afa6a2c84ceec5a3fe4d4d3f1d28608
Rob <robmoore121@gmail.com>
2018-10-28 10:28:51 +0000

    ReadingQuestion shows the givens of every question

--- frontend/src/main/ReadingQuestion.js
+++ frontend/src/main/ReadingQuestion.js
@@ -4,10 +4,21 @@ import React, {Component} from "react"
 import '../styles/Question.css'
 
 export default class ReadingQuestion extends Component {
+    questions() {
+        return this.props.q.questions.map((q, i) => {
+            return <div id={"question-" + String(i+1) + "-given"} key={"question-" + String(i+1) + "-given"}>
+                {q.given}
+            </div>
+        })
+    }
+
     render() {
         return [
             <div id="question-source" key="question-source">
                 {this.props.q.source}
+            </div>,
+            <div key="questions">
+                {this.questions()}
             </div>
         ]
     }
--- frontend/src/test/unit/ReadingQuestion.test.js
+++ frontend/src/test/unit/ReadingQuestion.test.js
@@ -9,7 +9,7 @@ import {configureAdapter} from "../utils"
 
 configureAdapter()
 
-it('Shows the source in a reading question', () => {
+it('Shows the source', () => {
     let q = {
         type: 2,
         source: "Vlad went to the kitchen and got some cake",
@@ -21,4 +21,19 @@ it('Shows the source in a reading question', () => {
     let testRQ = shallow(<ReadingQuestion q={q} />)
 
     expect(testRQ.find("#question-source").text()).toBe("Vlad went to the kitchen and got some cake")
+})
+
+it('Shows all the questions', () => {
+    let q = {
+        type: 2,
+        source: "Vlad went to the kitchen and got some cake",
+        questions: [
+            {given: "Where did Vlad go?", answer: "Kitchen"},
+            {given: "What did he get there?", answer: "Cake"}
+        ]
+    }
+    let testRQ = shallow(<ReadingQuestion q={q} />)
+
+    expect(testRQ.find("#question-1-given").text()).toBe("Where did Vlad go?")
+    expect(testRQ.find("#question-2-given").text()).toBe("What did he get there?")
 })
