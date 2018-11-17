925515f5ef6375c002867196026bc261fb54f5aa
Rob <robmoore121@gmail.com>
2018-11-17 21:48:06 +0000

    Upgraded things to squash security warning. Squashed 'a tag without href' warning by changing to span

--- frontend/src/main/Question.js
+++ frontend/src/main/Question.js
@@ -6,22 +6,22 @@ import '../styles/Question.css'
 
 export function submitForMarkingButton(setMark) {
     return (
-        <a id="submit-for-marking-button" className="mark-continue-button" key="submit-for-marking-button"
+        <span id="submit-for-marking-button" className="mark-continue-button" key="submit-for-marking-button"
            onClick={setMark}>
            Mark
-        </a>
+        </span>
     )
 }
 
 function _continueButton(completionListener, enabled) {
     let className = enabled ? "mark-continue-button" : "mark-continue-button-disabled"
     return (
-        <a id="continue-button" className={className} key="continue-button"
+        <span id="continue-button" className={className} key="continue-button"
            onClick={() => {
                completionListener()
            }}>
            Continue
-        </a>
+        </span>
     )
 }
 
