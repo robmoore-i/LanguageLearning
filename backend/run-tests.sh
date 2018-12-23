f27e1086e820b886bed8f885432491511639f34c
Rob <robmoore121@gmail.com>
2018-12-23 18:05:18 +0000

    Tests now sorted out

--- backend/run-tests.sh
+++ backend/run-tests.sh
@@ -1,12 +1,17 @@
 if [ "$1" == "r" ]
 then
   ./assemble.sh
+  cd test
   python3 test.py
-  exit $?
+  exitCode=$?
+  cd ..
+  exit $exitCode
 elif [ "$1" == "a" ]
 then
   python3 test.py
-  exit $?
+  exitCode=$?
+  cd ..
+  exit $exitCode
 else
   echo "USAGE: $0 [r|a]"
   echo "a => don't rebuild before testing because it's (a)lready built."
