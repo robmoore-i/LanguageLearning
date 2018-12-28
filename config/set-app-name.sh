937db63a7f6469369e83ef702a51f3fa4038bb80
Rob <robmoore121@gmail.com>
2018-12-28 11:31:06 +0000

    Change set-app-name script to use underscores in the package.json

--- config/set-app-name.sh
+++ config/set-app-name.sh
@@ -7,7 +7,9 @@ sed -i "22s/.*/    <title>${APP_APP_NAME}<\/title>/" ../frontend/public/index.ht
 
 # Change name in line 2 of frontend/package.json
 
-sed -i "2s/.*/  \"name\": \"${APP_APP_NAME}\",/" ../frontend/package.json
+packageJsonAppName=`echo $APP_APP_NAME | sed -e 's/ /_/g'`
+
+sed -i "2s/.*/  \"name\": \"${packageJsonAppName}\",/" ../frontend/package.json
 
 set +e
 set +x
