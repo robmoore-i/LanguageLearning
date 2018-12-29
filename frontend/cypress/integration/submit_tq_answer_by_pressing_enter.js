6fdc396b4f27ef17daa13f475fc6ac8668ca00da
Rob <robmoore121@gmail.com>
2018-12-29 16:09:23 +0000

    TQ test accounts for potential variation in the expected answer due to shuffler

--- frontend/cypress/integration/submit_tq_answer_by_pressing_enter.js
+++ frontend/cypress/integration/submit_tq_answer_by_pressing_enter.js
@@ -1,7 +1,17 @@
 describe('Completing a TQ by pressing enter instead of clicking the button', function() {
     it('Completes the TQ by pressing enter', function() {
         cy.visit('http://localhost:3000/courses/French/Animals')
-            .get('#answer-input-textbox').type('La Souris')
+            .get('#question-given').then((elem) => {
+                let text = elem.text().trim()
+                if (text === "La Souris") {
+                    return cy.get('#answer-input-textbox').type('Mouse')
+                } else if (text === "Mouse") {
+                    return cy.get('#answer-input-textbox').type('La Souris')
+                } else {
+                    console.log("Expected #question-given text, \"" + text + "\", to equal either \"La Souris\" or \"Mouse\"")
+                    assert.isTrue(false)
+                }
+            })
             .type("{enter}")
             .type("{enter}")
             .get('#lesson-accuracy-number').should('have.text', "100%")
