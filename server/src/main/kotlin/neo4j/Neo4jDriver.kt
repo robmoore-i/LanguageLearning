8c691e8bf27b0814ad49e2359f6627841e199033
Rob <robmoore121@gmail.com>
2019-01-19 12:04:38 +0000

    make Neo4jDriver fields private

--- server/src/main/kotlin/neo4j/Neo4jDriver.kt
+++ server/src/main/kotlin/neo4j/Neo4jDriver.kt
@@ -5,8 +5,8 @@ import org.neo4j.driver.v1.GraphDatabase
 import org.neo4j.driver.v1.Record
 
 class Neo4jDriver(user: String, password: String, boltPort: Int) {
-    val uri = "bolt://$user:$password@localhost:$boltPort"
-    val driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password))
+    private val uri = "bolt://$user:$password@localhost:$boltPort"
+    private val driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password))
 
     fun query(query: String): MutableIterable<Record> {
         return driver.session().readTransaction { tx -> tx.run(query).list() }
