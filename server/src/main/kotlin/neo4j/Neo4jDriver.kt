933d3318f0155f0a143e2351e3073e83c31be6ef
Rob <robmoore121@gmail.com>
2019-01-19 12:03:38 +0000

    Can query with params using Neo4jDriver

--- server/src/main/kotlin/neo4j/Neo4jDriver.kt
+++ server/src/main/kotlin/neo4j/Neo4jDriver.kt
@@ -4,12 +4,15 @@ import org.neo4j.driver.v1.AuthTokens
 import org.neo4j.driver.v1.GraphDatabase
 import org.neo4j.driver.v1.Record
 
-
 class Neo4jDriver(user: String, password: String, boltPort: Int) {
     val uri = "bolt://$user:$password@localhost:$boltPort"
     val driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password))
 
     fun query(query: String): MutableIterable<Record> {
         return driver.session().readTransaction { tx -> tx.run(query).list() }
+    }
+
+    fun queryWithParams(query: String, params: Map<String, String>): MutableIterable<Record> {
+        return driver.session().readTransaction { tx -> tx.run(query, params).list() }
     }
 }
--- server/src/test/kotlin/integration/Neo4jDriverTest.kt
+++ server/src/test/kotlin/integration/Neo4jDriverTest.kt
@@ -16,4 +16,21 @@ class Neo4jDriverTest {
 
         assertThat(courseNames, hasItem("\"Georgian\""))
     }
+
+    @Test
+    fun canQueryForGeorgianHelloLessonFromNeo4j() {
+        val neo4jDriver = Neo4jDriver("neo4j", "zuhlke", 7687)
+        val records = neo4jDriver.queryWithParams(
+            "MATCH (tl:TopicLesson {name: {name}})-[r:HAS_QUESTION]->(q) RETURN q,r.index",
+            mapOf(
+                "name" to "Peace Corps Georgia: Hello"
+            )
+        )
+
+        val tqGivenValues = records
+            .filter { r -> r[0].asNode().hasLabel("TranslationQuestion") }
+            .map { r -> r[0]["given"].toString() }
+
+        assertThat(tqGivenValues, hasItem("\"Hello\""))
+    }
 }
