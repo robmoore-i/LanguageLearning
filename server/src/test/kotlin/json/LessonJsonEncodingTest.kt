4d7e2fc05f5f8c4515a65838bcd54412990e6ea8
Rob Moore <robmoore121@gmail.com>
2019-01-27 16:03:30 +0000

    big ugly integration test for encoding a lesson with tq, mcq and rq. lazy but oh well.

--- server/src/test/kotlin/json/LessonJsonEncodingTest.kt
+++ server/src/test/kotlin/json/LessonJsonEncodingTest.kt
@@ -1,10 +1,9 @@
 package json
 
 import com.fasterxml.jackson.databind.JsonNode
-import model.Lesson
-import model.Question
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
+import model.*
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.http4k.format.Jackson
 import org.http4k.unquoted
 import org.junit.Test
@@ -15,6 +14,47 @@ class LessonJsonEncodingTest {
     private val encoder = JsonEncoder()
 
     @Test
+    fun canEncodeLessonWithThreeTypesOfQuestion() {
+        val questions = listOf<Question>(
+            TranslationQuestion("tq-given", listOf("tq-answer-1", "tq-answer-2")),
+            MultipleChoiceQuestion("is 'a'", mapOf('a' to "a", 'b' to "b", 'c' to "c", 'd' to "d"), 'a'),
+            ReadingQuestion(listOf(ReadingSubQuestion("rsq-given", "rsq-answer")))
+        )
+        val lesson = Lesson("Georgian", "lesson-name", 0, questions)
+
+        val encoded = encoder.encodeLesson(lesson)
+
+        val node: JsonNode = json.parse(encoded)
+
+        assertThat(node["questions"].size(), equalTo(3))
+
+        val tq = node["questions"][0]
+        assertThat(tq["type"].asInt(), equalTo(0))
+        assertThat(tq["index"].asInt(), equalTo(0))
+        assertThat(tq["given"].toString().unquoted(), equalTo("tq-given"))
+        assertThat(tq["answers"][0].toString().unquoted(), equalTo("tq-answer-1"))
+        assertThat(tq["answers"][1].toString().unquoted(), equalTo("tq-answer-2"))
+
+        val mcq = node["questions"][1]
+        assertThat(mcq["type"].asInt(), equalTo(1))
+        assertThat(mcq["index"].asInt(), equalTo(1))
+        assertThat(mcq["question"].toString().unquoted(), equalTo("is 'a'"))
+        assertThat(mcq["a"].toString().unquoted(), equalTo("a"))
+        assertThat(mcq["b"].toString().unquoted(), equalTo("b"))
+        assertThat(mcq["c"].toString().unquoted(), equalTo("c"))
+        assertThat(mcq["d"].toString().unquoted(), equalTo("d"))
+        assertThat(mcq["answer"].toString().unquoted(), equalTo("a"))
+
+        val rq = node["questions"][2]
+        assertThat(rq["type"].asInt(), equalTo(2))
+        assertThat(rq["index"].asInt(), equalTo(2))
+
+        val rsq = rq["questions"][0]
+        assertThat(rsq["given"].toString().unquoted(), equalTo("rsq-given"))
+        assertThat(rsq["answer"].toString().unquoted(), equalTo("rsq-answer"))
+    }
+
+    @Test
     fun canEncodeLessonWithNoQuestions() {
         val questions = listOf<Question>()
         val lesson = Lesson("Georgian", "lesson-name", 0, questions)
@@ -22,7 +62,7 @@ class LessonJsonEncodingTest {
         val encoded = encoder.encodeLesson(lesson)
 
         val node: JsonNode = json.parse(encoded)
-        MatcherAssert.assertThat(node["questions"].size(), CoreMatchers.equalTo(0))
+        assertThat(node["questions"].size(), equalTo(0))
     }
 
     @Test
@@ -33,7 +73,7 @@ class LessonJsonEncodingTest {
         val encoded = encoder.encodeLesson(lesson)
 
         val node: JsonNode = json.parse(encoded)
-        MatcherAssert.assertThat(node["courseName"].toString().unquoted(), CoreMatchers.equalTo("Georgian"))
+        assertThat(node["courseName"].toString().unquoted(), equalTo("Georgian"))
     }
 
     @Test
@@ -44,7 +84,7 @@ class LessonJsonEncodingTest {
         val encoded = encoder.encodeLesson(lesson)
 
         val node: JsonNode = json.parse(encoded)
-        MatcherAssert.assertThat(node["name"].toString().unquoted(), CoreMatchers.equalTo("lesson-name"))
+        assertThat(node["name"].toString().unquoted(), equalTo("lesson-name"))
     }
 
     @Test
@@ -55,6 +95,6 @@ class LessonJsonEncodingTest {
         val encoded = encoder.encodeLesson(lesson)
 
         val node: JsonNode = json.parse(encoded)
-        MatcherAssert.assertThat(node["index"].asInt(), CoreMatchers.equalTo(0))
+        assertThat(node["index"].asInt(), equalTo(0))
     }
 }
