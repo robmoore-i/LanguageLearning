0700239e45098d6cf496ac2ff836314ad8345588
Rob Moore <robmoore121@gmail.com>
2019-01-27 13:52:15 +0000

    Wrote another basic test for CourseMetadata initialisation from neo4j value pairs.

--- server/src/test/kotlin/model/CourseMetadataTest.kt
+++ server/src/test/kotlin/model/CourseMetadataTest.kt
@@ -1,11 +1,29 @@
 package model
 
+import neo4j.intValue
+import neo4j.stringValue
 import org.hamcrest.CoreMatchers.equalTo
 import org.junit.Assert.assertThat
 import org.junit.Test
 import org.neo4j.driver.v1.Value
 
 class CourseMetadataTest {
+    @Test
+    fun canInitialiseFromNeo4jValuePairsWithAFewLessons() {
+        val valuePairs: List<Pair<Value, Value>> = listOf(
+            Pair(stringValue("lesson-name-1"), intValue(0)),
+            Pair(stringValue("lesson-name-2"), intValue(1)),
+            Pair(stringValue("lesson-name-3"), intValue(2))
+        )
+
+        val courseMetadata = CourseMetadata.fromNeo4jValuePairs(valuePairs)
+
+        assertThat(courseMetadata.numberOfLessons(), equalTo(3))
+        assertThat(courseMetadata.titleOfLessonAtIndex(0), equalTo("lesson-name-1"))
+        assertThat(courseMetadata.titleOfLessonAtIndex(1), equalTo("lesson-name-2"))
+        assertThat(courseMetadata.titleOfLessonAtIndex(2), equalTo("lesson-name-3"))
+    }
+
     @Test
     fun canInitialiseFromNeo4jValuePairsWithNoLessons() {
         val valuePairs: List<Pair<Value, Value>> = listOf()
