c97e8b33092fbcf7e39ba720b3376eda15327548
Rob Moore <robmoore121@gmail.com>
2019-01-27 13:15:36 +0000

    add secondary constructor to TranslationQuestion to allow single answer or multiple answers

--- server/src/main/kotlin/model/TranslationQuestion.kt
+++ server/src/main/kotlin/model/TranslationQuestion.kt
@@ -3,14 +3,16 @@ package model
 import com.fasterxml.jackson.databind.JsonNode
 import org.http4k.format.Jackson
 
-data class TranslationQuestion(val given: String, val answer: String) : Question {
+data class TranslationQuestion(val given: String, val answers: List<String>) : Question {
+    constructor(given: String, answer: String) : this(given, listOf(answer))
+
     val json = Jackson
 
     override fun jsonify(): JsonNode {
         return json {
             obj(
                 "given" to string(given),
-                "answer" to string(answer)
+                "answer" to string(answers[0])
             )
         }
     }
