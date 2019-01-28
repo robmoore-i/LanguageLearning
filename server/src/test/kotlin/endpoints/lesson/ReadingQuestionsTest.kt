839cc4df923c1434d60e39cc9b42c9430e8eed82
Rob Moore <robmoore121@gmail.com>
2019-01-28 22:15:23 +0000

    Add test for getting RQ with RSQ having 4 answers

--- server/src/test/kotlin/endpoints/lesson/ReadingQuestionsTest.kt
+++ server/src/test/kotlin/endpoints/lesson/ReadingQuestionsTest.kt
@@ -6,6 +6,7 @@ import logger.ServerLogger
 import neo4j.Neo4jDatabaseAdaptor
 import neo4j.Neo4jDriver
 import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.hasItem
 import org.hamcrest.MatcherAssert.assertThat
 import org.http4k.client.JavaHttpClient
 import org.http4k.core.Method
@@ -15,6 +16,7 @@ import org.http4k.format.Jackson
 import org.http4k.server.Http4kServer
 import org.http4k.unquoted
 import org.junit.After
+import org.junit.Assert.assertFalse
 import org.junit.Before
 import org.junit.Test
 import server.LegacyServer
@@ -113,6 +115,7 @@ class ReadingQuestionsTest {
         val rsq = subquestions[0]
         assertThat(rsq["given"].toString().unquoted(), equalTo("What does 'საქართველო' mean in English?"))
         assertThat(rsq["answer"].toString().unquoted(), equalTo("Georgia"))
+        assertFalse(rsq.has("answers"))
     }
 
     @Test
@@ -138,6 +141,38 @@ class ReadingQuestionsTest {
         assertThat(rq["type"].asInt(), equalTo(2))
         assertThat(rq["index"].asInt(), equalTo(0))
         assertThat(rq["extract"].toString().unquoted(), equalTo("file-extract"))
+    }
+
+    @Test
+    fun canGetAnRsqWithMultipleAnswers() {
+        neo4jDriver.session().let { session ->
+            val query = """
+                CREATE (c:Course {name: "c", image: "img.png"})-[:HAS_TOPIC_LESSON {index: 0}]->(l:TopicLesson {name: "RQ"})
+                CREATE (l)-[:HAS_QUESTION {index: 0}]->(rq:Question:ReadingQuestion {extractInline: "inline-extract"})
+                CREATE (rq)-[:HAS_SUBQUESTION {index: 0}]->(rsq:ReadingSubQuestion {given:"What does 'ის მოხალისეა' mean in English?", answers:["She is a volunteer", "She's a volunteer", "He is a volunteer", "He's a volunteer"]})
+                RETURN l,rq,rsq,c;
+                """
+
+            session.run(query)
+            session.close()
+        }
+
+        val responseJson = lessonRequestJson("RQ")
+        val questions = responseJson["questions"]
+        assertThat(questions.size(), equalTo(1))
+
+        val rq = questions[0]
+        val subquestions = rq["questions"]
+
+        val rsq = subquestions[0]
+        assertThat(rsq["given"].toString().unquoted(), equalTo("What does 'ის მოხალისეა' mean in English?"))
+        val answers = rsq["answers"].map { s -> s.toString().unquoted() }
+        assertThat(answers, hasItem("She is a volunteer"))
+        assertThat(answers, hasItem("She's a volunteer"))
+        assertThat(answers, hasItem("He is a volunteer"))
+        assertThat(answers, hasItem("He's a volunteer"))
+        assertThat(answers.size, equalTo(4))
+        assertFalse(rsq.has("answer"))
     }
 
     private fun lessonRequest(lessonName: String): Response {
