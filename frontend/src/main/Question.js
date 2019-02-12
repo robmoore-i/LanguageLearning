3940cd0436c45a2b871cbe76c873b7845a5293f0
romo <robert.moore@zuhlke.com>
2019-02-12 13:51:38 +0000

    rename function param to onClick

--- frontend/src/main/Question.js
+++ frontend/src/main/Question.js
@@ -1,32 +1,31 @@
 // React
 import React from "react"
-
 // Resources
 import '../styles/Question.css'
 
-export function submitForMarkingButton(setMark) {
+export function submitForMarkingButton(onClick) {
     return (
         <span id="submit-for-marking-button" className="mark-continue-button" key="submit-for-marking-button"
-           onClick={setMark}>
+              onClick={onClick}>
            Mark
         </span>
     )
 }
 
-function _continueButton(completionListener, enabled) {
+function _continueButton(onClick, enabled) {
     let className = enabled ? "mark-continue-button" : "mark-continue-button-disabled"
     return (
         <span id="continue-button" className={className} key="continue-button"
            onClick={() => {
-               completionListener()
+               onClick()
            }}>
            Continue
         </span>
     )
 }
 
-export function continueButton(completionListener) {
-    return _continueButton(completionListener, true)
+export function continueButton(onClick) {
+    return _continueButton(onClick, true)
 }
 
 export const disabledContinueButton = _continueButton(() => {}, false)
