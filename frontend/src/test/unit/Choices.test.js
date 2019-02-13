b5af2f3c21494b2fa141bb6d105d35f72e2607f8
romo <robert.moore@zuhlke.com>
2019-02-13 17:23:33 +0000

    Add a couple of unit tests for creating a bar separated string in Choices.

--- frontend/src/test/unit/Choices.test.js
+++ frontend/src/test/unit/Choices.test.js
@@ -9,3 +9,15 @@ it("'4' is a choice key when there's 4 choices", () => {
     let c = Choices(4)
     expect(c.isChoiceKey("4")).toBe(true)
 })
+
+it("Can create a bar separated string given a question object", () => {
+    let c = Choices(4)
+    let barSeparatedString = c.toBarSeparatedString({a: "a", b: "b", c: "c", d: "d"})
+    expect(barSeparatedString).toEqual("|a|b|c|d|")
+})
+
+it("Can create a bar separated string given a question object of 3 questions", () => {
+    let c = Choices(3)
+    let barSeparatedString = c.toBarSeparatedString({a: "a", b: "b", c: "c"})
+    expect(barSeparatedString).toEqual("|a|b|c|")
+})
