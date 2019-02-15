ba8a367710415ddb6ce6e61e163200ce6b2015fb
romo <robert.moore@zuhlke.com>
2019-02-15 14:34:29 +0000

    delete scaffolding method for the QuestionQueue refactor

--- frontend/src/main/QuestionQueue.js
+++ frontend/src/main/QuestionQueue.js
@@ -1,18 +1,5 @@
 export function QuestionQueue(questionQueue, currentIndex) {
     return {
-        repositionIncorrectlyAnsweredQuestion: (index) => {
-            let incorrectQuestion = questionQueue[index]
-            let currentQuestionType = incorrectQuestion.type
-            let i = index
-            while (i < questionQueue.length && questionQueue[i].type === currentQuestionType) {
-                i += 1
-            }
-
-            let head = questionQueue.slice(0, i)
-            let tail = questionQueue.slice(i, questionQueue.length)
-            return head.concat([incorrectQuestion]).concat(tail)
-        },
-
         count: () => questionQueue.length,
 
         get: (i) => questionQueue[i],
@@ -46,6 +33,8 @@ export function QuestionQueue(questionQueue, currentIndex) {
             let newQuestionQueueList = head.concat([incorrectQuestion]).concat(tail)
 
             return QuestionQueue(newQuestionQueueList, currentIndex + 1)
-        }
+        },
+
+        toList: () => questionQueue
     }
 }
