68abce34ce4a67160c73bef658be62f9b017a98a
romo <robert.moore@zuhlke.com>
2019-02-16 23:02:30 +0000

    Added a comment^

--- server/src/test/kotlin/endpoints/TestServerClient.kt
+++ server/src/test/kotlin/endpoints/TestServerClient.kt
@@ -2,6 +2,7 @@ package endpoints
 
 import org.http4k.core.Response
 
+// NB. As long as TDD is employed, this interface will always mirror that of the ServerApi class.
 interface TestServerClient {
     fun courses(): Response
     fun courseMetadata(courseName: String): Response
