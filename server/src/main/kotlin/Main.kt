944c515160153c7beb76a5c737334d27e88abfe5
romo <robert.moore@zuhlke.com>
2019-02-16 23:28:08 +0000

    rename variable^

--- server/src/main/kotlin/Main.kt
+++ server/src/main/kotlin/Main.kt
@@ -14,13 +14,11 @@ fun main(args: Array<String>) {
     val environmentLoader = EnvironmentLoader(System::getenv)
     val environment = environmentLoader.getEnvironment()
 
-    val logger = ServerLogger()
-
     val neo4jDriver = Neo4jDriver(environment.neo4jUser, environment.neo4jPassword, environment.neo4jPort)
     val neo4jDatabaseAdaptor = Neo4jDatabaseAdaptor(neo4jDriver, environment.imagesPath, environment.extractsPath)
 
     val serverHttpApi = ServerHttpApi(ServerApi(neo4jDatabaseAdaptor), HttpResponseFactory(environment.frontendPort))
 
-    val server = HttpServer(serverHttpApi, environment.serverPort, logger)
-    server.start()
+    val httpServer = HttpServer(serverHttpApi, environment.serverPort, ServerLogger())
+    httpServer.start()
 }
