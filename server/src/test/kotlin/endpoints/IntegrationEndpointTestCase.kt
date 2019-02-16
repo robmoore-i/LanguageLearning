861a802b545b518a86314997f2fc900a1f49322a
romo <robert.moore@zuhlke.com>
2019-02-16 22:34:28 +0000

    Inline HttpTestRequester to IntegrationEndpointTestCase.kt

--- server/src/test/kotlin/endpoints/IntegrationEndpointTestCase.kt
+++ server/src/test/kotlin/endpoints/IntegrationEndpointTestCase.kt
@@ -7,11 +7,15 @@ import model.CourseMetadata
 import model.Lesson
 import neo4j.Neo4jDatabaseAdaptor
 import neo4j.Neo4jDriver
+import org.http4k.client.JavaHttpClient
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
 import server.Server
 
 private val environment = EnvironmentLoader(System::getenv).getEnvironment()
 
-private val testDatabaseAdaptor = object : TestDatabaseAdaptor {
+private val neo4jDatabaseAdaptor = object : TestDatabaseAdaptor {
     val neo4jDriver = Neo4jDriver(environment.neo4jUser, environment.neo4jPassword, environment.neo4jPort)
     val neo4jDatabaseAdaptor = Neo4jDatabaseAdaptor(
             neo4jDriver,
@@ -43,18 +47,39 @@ private val testDatabaseAdaptor = object : TestDatabaseAdaptor {
     }
 }
 
+private val httpRequester = object : TestRequester {
+    private val client = JavaHttpClient()
+    private val serverUrl = "http://localhost:${environment.serverPort}"
+
+    override fun courseMetadataRequest(courseName: String): Response {
+        val request = Request(Method.GET, "$serverUrl/coursemetadata?course=$courseName")
+        return client.invoke(request)
+    }
+
+    override fun coursesRequest(): Response {
+        val request = Request(Method.GET, "$serverUrl/courses")
+        return client.invoke(request)
+    }
+
+    override fun lessonRequest(courseName: String, lessonName: String): Response {
+        val request = Request(
+                Method.POST,
+                "$serverUrl/lesson"
+        ).body("{\"lessonName\":\"$lessonName\",\"courseName\":\"$courseName\"}")
+        return client.invoke(request)
+    }
+}
+
 private val server: Server = Server(
         environment.serverPort,
-        testDatabaseAdaptor,
+        neo4jDatabaseAdaptor,
         environment.frontendPort,
         ServerLogger()
 )
 
-private val testRequester = HttpTestRequester(environment)
-
 open class IntegrationEndpointTestCase : EndpointTestCase(
         environment,
-        testDatabaseAdaptor,
-        testRequester,
+        neo4jDatabaseAdaptor,
+        httpRequester,
         server
 )
