04f965dde2b01e0d5db30bfd5efc27deec9c840a
romo <robert.moore@zuhlke.com>
2019-02-16 21:04:57 +0000

    remove unused field^

--- server/src/test/kotlin/endpoints/HttpTestRequester.kt
+++ server/src/test/kotlin/endpoints/HttpTestRequester.kt
@@ -5,13 +5,10 @@ import org.http4k.client.JavaHttpClient
 import org.http4k.core.Method
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.format.Jackson
 
 class HttpTestRequester(environment: AppEnvironment) : TestRequester {
     private val client = JavaHttpClient()
     private val serverUrl = "http://localhost:${environment.serverPort}"
-
-    val json = Jackson
 
     override fun courseMetadataRequest(courseName: String): Response {
         val request = Request(Method.GET, "$serverUrl/coursemetadata?course=$courseName")
